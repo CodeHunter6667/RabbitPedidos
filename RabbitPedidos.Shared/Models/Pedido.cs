e39f029f18edad70301bfaae56e927cea2044c54
Rafael Ehlert <ra.faehlert@hotmail.com>
2025-11-20 17:31:33 -0300

    feat: criado model de Pedido

---
+++ RabbitPedidos.Shared/Models/Pedido.cs
@@ -0,0 +1,12 @@
+﻿using RabbitPedidos.Shared.Enums;
+
+namespace RabbitPedidos.Shared.Models;
+
+public class Pedido
+{
+    public Guid Id { get; set; }
+    public string NomeCliente { get; set; } = string.Empty;
+    public string Descricao { get; set; } = string.Empty;
+    public decimal Valor { get; set; }
+    public EPedidoStatus Status { get; set; }
+}
