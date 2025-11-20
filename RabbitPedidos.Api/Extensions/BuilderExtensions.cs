ff47352499213d4d11d416b2cee04be4919f2129
Rafael Ehlert <ra.faehlert@hotmail.com>
2025-11-20 17:44:46 -0300

    feat: criado extensions do WebbApplicationBuilder

---
+++ RabbitPedidos.Api/Extensions/BuilderExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RabbitPedidos.Api.Data;
+using RabbitPedidos.Shared.Commons;
+
+namespace RabbitPedidos.Api.Extensions;
+
+public static class BuilderExtensions
+{
+    public static void AddConfigurations(this WebApplicationBuilder builder)
+    {
+        Configurations.ConnectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? string.Empty;
+    }
+
+    public static void AddDbContext(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddDbContext<PedidosDbContext>(options =>
+        {
+            options.UseMySQL(Configurations.ConnectionString);
+        });
+    }
+}
