59cc6a3dbe0fddb77682707f1924154458646a0b
Rafael Ehlert <ra.faehlert@hotmail.com>
2025-11-20 17:44:02 -0300

    feat: criado DbContexto da Api

---
+++ RabbitPedidos.Api/Data/PedidosDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RabbitPedidos.Shared.Models;
+using System.Reflection;
+
+namespace RabbitPedidos.Api.Data;
+
+public class PedidosDbContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<Pedido> Pedidos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
