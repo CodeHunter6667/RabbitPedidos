da8361d4edb1adde3fceb5c7495b6d5ac2d99636
Rafael Ehlert <ra.faehlert@hotmail.com>
2025-11-23 13:00:20 -0300

    feat: feito mapeamento e migracao para o banco de dados

---
+++ RabbitPedidos.Api/Data/Mapping/PedidoMapping.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RabbitPedidos.Shared.Models;
+
+namespace RabbitPedidos.Api.Data.Mapping;
+
+public class PedidoMapping : IEntityTypeConfiguration<Pedido>
+{
+    public void Configure(EntityTypeBuilder<Pedido> builder)
+    {
+        builder.ToTable("Pedidos");
+
+        builder.HasKey(p => p.Id);
+
+        builder.Property(p => p.NomeCliente)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(p => p.Descricao)
+            .IsRequired()
+            .HasMaxLength(500);
+
+        builder.Property(p => p.Valor)
+            .IsRequired()
+            .HasColumnType("DECIMAL(18,2)");
+
+        builder.Property(p => p.Status)
+            .IsRequired()
+            .HasColumnType("SMALLINT");
+    }
+}
---
+++ RabbitPedidos.Api/Migrations/20251123145612_v1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RabbitPedidos.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class v1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Pedidos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false),
+                    NomeCliente = table.Column<string>(type: "varchar(100)", maxLength: 100, nullable: false),
+                    Descricao = table.Column<string>(type: "varchar(500)", maxLength: 500, nullable: false),
+                    Valor = table.Column<decimal>(type: "DECIMAL(18,2)", nullable: false),
+                    Status = table.Column<short>(type: "SMALLINT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pedidos", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pedidos");
+        }
+    }
+}
