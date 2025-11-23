4fa3f706d28346128bdc7fc740c5cc9c98b9b6e7
Rafael Ehlert <ra.faehlert@hotmail.com>
2025-11-23 13:01:54 -0300

    feat: criado controller de Pedido

---
+++ RabbitPedidos.Api/Controllers/PedidoController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RabbitPedidos.Api.Services.Interfaces;
+using RabbitPedidos.Shared.DTOs;
+
+namespace RabbitPedidos.Api.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class PedidoController(IPedidoService service) : ControllerBase
+{
+
+    [HttpPost]
+    public async Task<IActionResult> CriarPedido(InsertPedidoDto dto)
+    {
+        var response = await service.CriarPedidoAsync(dto);
+        return CreatedAtAction(nameof(CriarPedido), response);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> ObterPedidos()
+    {
+        var response = await service.ObterPedidos();
+        return Ok(response);
+    }
+}
